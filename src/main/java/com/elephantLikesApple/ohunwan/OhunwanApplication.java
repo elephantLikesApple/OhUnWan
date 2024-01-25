cf98313d6fb303320f5e1163528845c631ae6073
elephantLikesApple <elephant890@naver.com>
2024-01-25 15:23:34 +0900

    Add : create project

---
+++ src/main/java/com/elephantLikesApple/ohunwan/OhunwanApplication.java
@@ -0,0 +1,13 @@
+package com.elephantLikesApple.ohunwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhunwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhunwanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elephantLikesApple/ohunwan/OhunwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elephantLikesApple.ohunwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhunwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
